0c023087dcd5182f1a97623be5057496ad2634c6
Andrew Sukach <andrewsukach@gmail.com>
2024-02-23 18:38:20 -0500

    additions to core runtime

--- cawk.h
+++ cawk.h
@@ -15,34 +15,107 @@ using val_t__ = std::variant<int64_t, double, std::string, vector_t__>;
 
 inline static struct {
   template <typename T__>
-  constexpr bool operator()(const T__ &x__) const noexcept {
-    return std::holds_alternative<int64_t>(x__);
+  [[nodiscard]] constexpr bool operator()(const auto &x__) const noexcept {
+    return std::holds_alternative<T__>(x__);
+  }
+} is{};
+
+inline static struct {
+  [[nodiscard]] constexpr bool operator()(const auto &x__) const noexcept {
+    return is.operator()<int64_t>(x__);
   }
 } is_int;
 
 inline static struct {
-  template <typename T__>
-  constexpr bool operator()(const T__ &x__) const noexcept {
+  [[nodiscard]] constexpr bool operator()(const auto &x__) const noexcept {
     return std::holds_alternative<double>(x__);
   }
 } is_double{};
 
 inline static struct {
-  template <typename T__>
-  constexpr bool operator()(const T__ &x__) const noexcept {
+  [[nodiscard]] constexpr bool operator()(const auto &x__) const noexcept {
     return std::holds_alternative<std::string>(x__);
   }
 } is_string{};
 
 inline static struct {
-  template <typename T__>
-  constexpr bool operator()(const T__ &x__) const noexcept {
+  [[nodiscard]] constexpr bool operator()(const auto &x__) const noexcept {
     return std::holds_alternative<vector_t__>(x__);
   }
 } is_vector{};
 
 inline static struct {
-  constexpr int64_t operator()(const val_t__ &v__) const noexcept {
+  template <typename T__>
+    requires std::is_same_v<T__, int64_t>
+  [[nodiscard]] constexpr bool
+  operator()(const std::string &s__) const noexcept {
+    try {
+      std::stoll(s);
+      return true;
+    } catch (...) {
+      return false;
+    }
+  }
+
+  template <typename T__>
+    requires std::is_same_v<T__, double>
+  [[nodiscard]] constexpr bool
+  operator()(const std::string &s__) const noexcept {
+    try {
+      std::stod(s);
+      return true;
+    } catch (...) {
+      return false;
+    }
+  }
+
+} is_string_conv{};
+
+inline static struct {
+  template <typename T__>
+  [[nodiscard]] constexpr bool operator()(const auto &x) const noexcept {
+    if constexpr (std::is_same_v<T__, int64_t>)
+      return is_int(x) || is_double(x) ||
+             is_string(x) && is_string_conv<int>(x);
+
+    if constexpr (std::is_same_v<T__, double>)
+      return is_int(x) || is_double(x) ||
+             is_string(x) && is_string_conv<double>(x);
+
+    if constexpr (std::is_same_v<T__, std::string>)
+      return !is_vector(x);
+
+    return true;
+  }
+} is_conv{};
+
+inline static struct {
+  [[nodiscard]] constexpr bool operator()(const auto &x) const noexcept {
+    return is_conv.operator()<int64_t>(x);
+  }
+} is_conv_int{};
+
+inline static struct {
+  [[nodiscard]] constexpr bool operator()(const auto &x) const noexcept {
+    return is_conv.operator()<double>(x);
+  }
+} is_conv_double{};
+
+inline static struct {
+  [[nodiscard]] constexpr bool operator()(const auto &x) const noexcept {
+    return is_conv.operator()<std::string>(x);
+  }
+} is_conv_string{};
+
+inline static struct {
+  [[nodiscard]] constexpr bool operator()(const auto &x) const noexcept {
+    return is_conv.operator()<vector_t__>(x);
+  }
+} is_conv_vector{};
+
+inline static struct {
+  [[nodiscard]] constexpr int64_t
+  operator()(const val_t__ &v__) const noexcept {
     if (is_int(v__))
       return std::get<int64_t>(v__);
     if (is_double(v__))
@@ -57,7 +130,7 @@ inline static struct {
     exit(EXIT_FAILURE);
   }
 
-  constexpr int64_t operator()(val_t__ &&v__) const noexcept {
+  [[nodiscard]] constexpr int64_t operator()(val_t__ &&v__) const noexcept {
     if (is_int(v__))
       return std::get<int64_t>(std::move(v__));
     if (is_double(v__))
@@ -75,7 +148,7 @@ inline static struct {
 } to_int{};
 
 inline static struct {
-  constexpr double operator()(const val_t__ &v__) noexcept {
+  [[nodiscard]] constexpr double operator()(const val_t__ &v__) const noexcept {
     if (is_int(v__))
       return static_cast<double>(std::get<int64_t>(v__));
     if (is_double(v__))
@@ -90,7 +163,7 @@ inline static struct {
     exit(EXIT_FAILURE);
   }
 
-  constexpr double operator()(val_t__ &&v__) noexcept {
+  [[nodiscard]] constexpr double operator()(val_t__ &&v__) const noexcept {
     if (is_int(v__))
       return static_cast<double>(std::get<int64_t>(std::move(v__)));
     if (is_double(v__))
@@ -106,82 +179,105 @@ inline static struct {
   }
 } to_double{};
 
-inline constexpr std::string to_string(const val_t__ &v__) noexcept {
-  if (is_int(v__))
-    return std::to_string(std::get<int64_t>(v__));
-  if (is_double(v__))
-    return std::to_string(std::get<double>(v__));
-  if (is_string(v__))
-    return std::get<std::string>(v__);
+inline static struct {
+  [[nodiscard]] constexpr std::string
+  operator()(const val_t__ &v__) const noexcept {
+    if (is_int(v__))
+      return std::to_string(std::get<int64_t>(v__));
+    if (is_double(v__))
+      return std::to_string(std::get<double>(v__));
+    if (is_string(v__))
+      return std::get<std::string>(v__);
 
-  std::cerr << "invalid string conversion." << std::endl;
-  exit(EXIT_FAILURE);
-}
+    std::cerr << "invalid string conversion." << std::endl;
+    exit(EXIT_FAILURE);
+  }
 
-inline constexpr std::string to_string(val_t__ &&v__) noexcept {
-  if (is_int(v__))
-    return std::to_string(std::get<int64_t>(v__));
-  if (is_double(v__))
-    return std::to_string(std::get<double>(v__));
-  if (is_string(v__))
-    return std::get<std::string>(v__);
+  [[nodiscard]] constexpr std::string operator()(val_t__ &&v__) const noexcept {
+    if (is_int(v__))
+      return std::to_string(std::get<int64_t>(std::move(v__)));
+    if (is_double(v__))
+      return std::to_string(std::get<double>(std::move(v__)));
+    if (is_string(v__))
+      return std::get<std::string>(std::move(v__));
 
-  std::cerr << "invalid string conversion." << std::endl;
-  exit(EXIT_FAILURE);
-}
+    std::cerr << "invalid string conversion." << std::endl;
+    exit(EXIT_FAILURE);
+  }
+} to_string{};
 
-inline constexpr vector_t__ to_vector(const val_t__ &v__) noexcept {
-  return is_vector(v__)   ? std::get<vector_t__>(v__)
-         : is_int(v__)    ? vector_t__{std::get<int64_t>(v__)}
-         : is_double(v__) ? vector_t__{std::get<double>(v__)}
-         : is_string(v__) ? vector_t__{std::get<std::string>(v__)}
-                          : vector_t__{};
-}
+inline static struct {
+  [[nodiscard]] constexpr vector_t__
+  operator()(const val_t__ &v__) const noexcept {
+    return is_vector(v__)   ? std::get<vector_t__>(v__)
+           : is_int(v__)    ? vector_t__{std::get<int64_t>(v__)}
+           : is_double(v__) ? vector_t__{std::get<double>(v__)}
+           : is_string(v__) ? vector_t__{std::get<std::string>(v__)}
+                            : vector_t__{};
+  }
 
-bool operator==(const val_t__ &x, const std::integral auto &y) noexcept {
-  return to_int(x) == y;
+  [[nodiscard]] constexpr vector_t__ operator()(val_t__ &&v__) const noexcept {
+    return is_vector(v__)   ? std::get<vector_t__>(std::move(v__))
+           : is_int(v__)    ? vector_t__{std::get<int64_t>(std::move(v__))}
+           : is_double(v__) ? vector_t__{std::get<double>(std::move(v__))}
+           : is_string(v__) ? vector_t__{std::get<std::string>(std::move(v__))}
+                            : vector_t__{};
+  }
+} to_vector{};
+
+bool operator==(const val_t__ &x__, const std::integral auto &y__) noexcept {
+  return to_int(x__) == y__;
 }
 
 /// Integer Operators.
 
-int64_t operator+(const val_t__ &x, const std::integral auto &y) noexcept {
-  if (auto z{to_int(x)}; z)
-    return *z + y;
-  std::cerr << "invalid operands for integer addition" << std::endl;
-  exit(EXIT_FAILURE);
+template <typename T__>
+  requires std::is_same_v<T__, val_t__>
+[[nodiscard]] inline static constexpr int64_t
+operator+(T__ &&x__, std::integral auto &&y) noexcept {
+  return to_int(x__) + y;
 }
 
-int64_t operator-(const val_t__ &x, const std::integral auto &y) noexcept {
-  if (auto z{to_int(x)}; z)
-    return *z - y;
-  std::cerr << "invalid operands for integer subtraction" << std::endl;
-  exit(EXIT_FAILURE);
+template <typename T__>
+  requires std::is_same_v<T__, val_t__>
+[[nodiscard]] inline static constexpr int64_t
+operator-(T__ &&x__, std::integral auto &&y) noexcept {
+  return to_int(x__) - y;
 }
 
-int64_t operator*(const val_t__ &x, const std::integral auto &y) noexcept {
-  if (auto z{to_int(x)}; z)
-    return *z * y;
-  std::cerr << "invalid operands for integer addition" << std::endl;
-  exit(EXIT_FAILURE);
+template <typename T__>
+  requires std::is_same_v<T__, val_t__>
+[[nodiscard]] inline static constexpr int64_t
+operator*(T__ &&x__, std::integral auto &&y) noexcept {
+  return to_int(x__) * y;
 }
 
-int64_t operator/(const val_t__ &x, const std::integral auto &y) noexcept {
-  if (auto z{to_int(x)}; z)
-    return *z / y;
-  std::cerr << "invalid operands for integer subtraction" << std::endl;
-  exit(EXIT_FAILURE);
+template <typename T__>
+  requires std::is_same_v<T__, val_t__>
+[[nodiscard]] inline static constexpr int64_t
+operator/(T__ &&x__, std::integral auto &&y) noexcept {
+  return to_int(x__) / y;
 }
 
 val_t__ operator+(const val_t__ &x, const val_t__ &y) noexcept {
   if (is_int(x)) {
-    if (auto z{to_int(y)}; z)
-      return x + *z;
+    return x + to_int(y);
   } else if (is_double(x)) {
-    if (auto z{to_double(y)}; z)
-      return x + *z;
+    x + to_double(y);
   } else if (is_string(x)) {
-    if (auto z{to_string(y)}; z)
-      return x + *z;
+    if (is_conv_int(x)) {
+      if (is_conv_int(y))
+        return to_int(x) + to_int(y);
+      if (is_conv_double(y))
+        return to_int(x) + to_double(y);
+      return to_string(x) + to_string(y);
+    }
+    if (is_conv_double(x)) {
+      if (is_conv_int(y))
+        return static_cast<int64_t>(to_double(y)) + to_int(y);
+      if (is_conv_double(y))
+        return to_double(x) + to_double(y);
+    }
   } else if (is_vector(x)) {
     auto z{to_vector(y)};
     vector_t__ w{std::get<vector_t__>(x)};
