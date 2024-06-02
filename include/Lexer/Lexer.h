0a485991ccf79ead776c79e95d3fad617d44c79f
Andrew Sukach <andrewsukach@gmail.com>
2024-06-02 02:09:38 -0400

    lexer rough draft

---
+++ include/Lexer/Lexer.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "Basic/TokenKinds.h"
+#include "Token.h"
+
+#include <string_view>
+#include <unordered_map>
+
+namespace cawk {
+class Lexer {
+  std::string_view::const_iterator BufferStart;
+  std::string_view::const_iterator BufferEnd;
+  std::string_view::const_iterator BufferPtr;
+
+  std::unordered_map<std::string_view, tok::TokenKind> Keywords;
+
+public:
+  Lexer(std::string_view Buffer)
+      : BufferStart(std::cbegin(Buffer)), BufferEnd(std::cend(Buffer)),
+        BufferPtr(BufferStart) {
+#define KEYWORD(ID, KEY) Keywords.emplace(#ID, tok::kw_##ID);
+#include "Basic/TokenKinds.def"
+#undef KEYWORD
+  }
+
+  void Next(Token &);
+
+private:
+  void FormToken(Token &, std::string_view::const_iterator, tok::TokenKind);
+  void Identifier(Token &);
+  void Number(Token &);
+  void String(Token &);
+};
+} // namespace cawk
---
+++ lib/Lexer/Lexer.cpp
@@ -0,0 +1,179 @@
+#include "Lexer/Lexer.h"
+#include "Basic/TokenKinds.h"
+
+namespace charinfo {
+inline bool IsWhitespace(char c) {
+  switch (c) {
+  default:
+    return false;
+  case ' ':
+  case '\t':
+  case '\f':
+  case '\v':
+  case '\r':
+  case '\n':
+    return true;
+  }
+}
+
+inline bool IsDigit(char c) {
+  switch (c) {
+  default:
+    return false;
+  case '0' ... '9':
+    return true;
+  }
+}
+
+inline bool IsLetter(char c) {
+  switch (c) {
+  default:
+    return false;
+  case 'A' ... 'Z':
+  case 'a' ... 'z':
+    return true;
+  }
+}
+} // namespace charinfo
+
+namespace cawk {
+void Lexer::Next(Token &T) {
+  for (; BufferPtr != BufferEnd && charinfo::IsWhitespace(*BufferPtr);
+       ++BufferPtr)
+    ;
+
+  if (BufferPtr == BufferEnd) {
+    T.Kind = tok::eof;
+    return;
+  }
+
+  if (charinfo::IsLetter(*BufferPtr)) {
+    auto End = BufferPtr + 1;
+    for (; charinfo::IsLetter(*End); ++End)
+      ;
+
+    std::string_view Name(BufferPtr, End);
+    tok::TokenKind Kind = tok::identifier;
+
+    FormToken(T, End, Kind);
+
+    return;
+  }
+
+  if (charinfo::IsDigit(*BufferPtr)) {
+    auto End = BufferPtr + 1;
+    for (; charinfo::IsDigit(*End); ++End)
+      ;
+
+    if (*BufferPtr == '.')
+      for (++BufferPtr; charinfo::IsDigit(*BufferPtr); ++BufferPtr)
+        ;
+
+    FormToken(T, End, tok::numeric_constant);
+
+    return;
+  }
+
+  switch (*BufferPtr) {
+#define CASE(CH, TOK)                                                          \
+  case CH:                                                                     \
+    FormToken(T, BufferPtr + 1, TOK);                                          \
+    break
+    CASE('[', tok::l_square);
+    CASE(']', tok::r_square);
+    CASE('(', tok::l_paren);
+    CASE(')', tok::r_paren);
+    CASE('?', tok::question);
+    CASE(':', tok::colon);
+    CASE(';', tok::semi);
+    CASE(',', tok::comma);
+    CASE('$', tok::dollar);
+    CASE('/', tok::slash);
+    CASE('~', tok::tilde);
+#undef CASE
+  case '&':
+    if (*(BufferPtr + 1) == '&')
+      FormToken(T, BufferPtr + 2, tok::ampamp);
+    else
+      FormToken(T, BufferPtr + 1, tok::amp);
+    break;
+  case '|':
+    if (*(BufferPtr + 1) == '|')
+      FormToken(T, BufferPtr + 2, tok::pipepipe);
+    else
+      FormToken(T, BufferPtr + 1, tok::pipe);
+    break;
+  case '=':
+    if (*(BufferPtr + 1) == '=')
+      FormToken(T, BufferPtr + 2, tok::equalequal);
+    else
+      FormToken(T, BufferPtr + 1, tok::equal);
+    break;
+  case '!':
+    if (*(BufferPtr + 1) == '=')
+      FormToken(T, BufferPtr + 2, tok::exclaimequal);
+    else if (*(BufferPtr + 1) == '~')
+      FormToken(T, BufferPtr + 2, tok::exclaimtilde);
+    else
+      FormToken(T, BufferPtr + 1, tok::exclaim);
+    break;
+  case '<':
+    if (*(BufferPtr + 1) == '=')
+      FormToken(T, BufferPtr + 2, tok::lessequal);
+    else
+      FormToken(T, BufferPtr + 1, tok::less);
+    break;
+  case '>':
+    if (*(BufferPtr + 1) == '>')
+      FormToken(T, BufferPtr + 2, tok::greatergreater);
+    else if (*(BufferPtr + 1) == '=')
+      FormToken(T, BufferPtr + 2, tok::greaterequal);
+    else
+      FormToken(T, BufferPtr + 1, tok::greater);
+    break;
+  case '+':
+    if (*(BufferPtr + 1) == '+')
+      FormToken(T, BufferPtr + 2, tok::plusplus);
+    else if (*(BufferPtr + 1) == '=')
+      FormToken(T, BufferPtr + 2, tok::plusequal);
+    else
+      FormToken(T, BufferPtr + 1, tok::plus);
+    break;
+  case '-':
+    if (*(BufferPtr + 1) == '-')
+      FormToken(T, BufferPtr + 2, tok::minusminus);
+    else if (*(BufferPtr + 1) == '=')
+      FormToken(T, BufferPtr + 2, tok::minusequal);
+    else
+      FormToken(T, BufferPtr + 1, tok::minus);
+    break;
+  case '*':
+    if (*(BufferPtr + 1) == '*')
+      FormToken(T, BufferPtr + 2, tok::starstar);
+    else if (*(BufferPtr + 1) == '=')
+      FormToken(T, BufferPtr + 2, tok::starequal);
+    else
+      FormToken(T, BufferPtr + 1, tok::star);
+    break;
+  case '^':
+    if (*(BufferPtr + 1) == '=')
+      FormToken(T, BufferPtr + 2, tok::caretequal);
+    else
+      FormToken(T, BufferPtr + 1, tok::caret);
+    break;
+  case '%':
+    if (*(BufferPtr + 1) == '=')
+      FormToken(T, BufferPtr + 2, tok::percentequal);
+    else
+      FormToken(T, BufferPtr + 1, tok::percent);
+    break;
+  default:
+    FormToken(T, BufferPtr + 1, tok::unknown);
+  }
+}
+
+void Lexer::Identifier(Token &T) {
+
+}
+
+} // namespace cawk
