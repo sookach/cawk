d1fbee8082c17627cff0ff1002adee51b8566921
Andrew Sukach <andrewsukach@gmail.com>
2024-07-13 21:50:07 -0400

    String/Print formatting improvements

--- include/Exec/Exec.h
+++ include/Exec/Exec.h
@@ -7,8 +7,11 @@
 #include "Support/Support.h"
 
 #include <algorithm>
+#include <cstddef>
 #include <cstdint>
+#include <iostream>
 #include <list>
+#include <numeric>
 #include <ranges>
 #include <utility>
 
@@ -153,6 +156,16 @@ private:
                                        })
                     .c_str());
     } else {
+      if (!std::empty(P->getArgs())) {
+        auto Args = std::ranges::fold_left(
+            P->getArgs() | std::views::drop(1), std::vector<Value>(),
+            [this](std::vector<Value> Args, Expr *E) {
+              Args.push_back(visit(E));
+              return Args;
+            });
+        auto S = format(visit(P->getArgs().front()).toString(), Args);
+        std::printf("%s", S.c_str());
+      }
     }
   }
 
@@ -351,69 +364,291 @@ CASE(Do, DoStmt);
     }
   }
 
-  std::string format(std::span<char> FormatString, std::vector<Value> Args) {
-    std::string String;
-    static constexpr auto MaxSize = 24'576;
-    String.reserve(MaxSize);
+  std::string format(std::string FormatString, std::vector<Value> Args) {
+    static constexpr std::string::size_type MaxSize = 10;
+    std::array<char, MaxSize> Buffer;
 
-    auto It = std::begin(FormatString);
+    auto It = std::begin(FormatString) + 1;
+    auto End = std::end(FormatString) - 1;
+    auto BufferIt = std::begin(Buffer);
 
     for (Value &A : Args) {
-      It = std::find(It, std::end(FormatString), '%');
-      if (It == std::cend(FormatString)) {
+      auto Next = std::find(It, End, '%');
+      if (Next == End) {
         assert(0 && "Invalid format string.");
         exit(EXIT_FAILURE);
       }
 
-      String.append(std::begin(FormatString), It);
+      std::copy(It, Next, BufferIt);
+      BufferIt += Next - It;
+      It = Next;
 
-      auto Next =
-          std::find_if(It + 1, std::end(FormatString), isConversionSpecifier);
-      if (Next == std::end(FormatString)) {
-        assert(0 && "Invalid format string.");
-        exit(EXIT_FAILURE);
-      }
+      assert(isConversionSpecifier(*(It + 1)));
 
-      if (*Next == '%') {
-        String.push_back('%');
+      if (*(It + 1) == '%') {
+        *(BufferIt++) = '%';
+        It += 2;
         continue;
       }
 
-      auto Save = std::exchange(*(Next + 1), '\0');
-      enum ConversionKind { CK_Number, CK_String, CK_Char };
+      auto WriteFormat = [&](const char *Fmt, auto Arg) {
+        auto N = std::snprintf(
+            BufferIt, MaxSize - (BufferIt - std::begin(Buffer)), Fmt, Arg);
+        if (N < 0) {
+          perror("");
+          exit(errno);
+        }
+        BufferIt += N;
+      };
 
-      auto Kind = *Next == 's' ? CK_String : *Next == 'c' ? CK_Char : CK_Number;
-
-      switch (*Next) {
+      switch (++It; *(It++)) {
       default:
-        std::snprintf(std::end(String).base(), MaxSize - std::size(String),
-                      It.base(), A.getNumber());
+        assert(0 && "Invalid format string.");
         break;
-      case 's':
-        std::snprintf(std::end(String).base(), MaxSize - std::size(String),
-                      It.base(), A.getString().c_str());
+      case '%':
+        *(BufferIt++) = '%';
         break;
       case 'c':
-        if (A.getKind() == Value::VK_Number)
-          std::snprintf(std::end(String).base(), MaxSize - std::size(String),
-                        It.base(), static_cast<char>(A.getNumber()));
-        else if (!std::empty(A.toString()))
-          std::snprintf(std::end(String).base(), MaxSize - std::size(String),
-                        It.base(), A.toString().front());
+        WriteFormat("%c", A.getKind() == Value::VK_Number
+                              ? A.getNumber()
+                              : static_cast<int>(A.getString().front()));
+        break;
+      case 's': {
+        auto S = A.toString();
+        WriteFormat("%s", S.c_str());
+        break;
       }
-
-      *(Next + 1) = Save;
-      Next += 2;
+      case 'd':
+      case 'i':
+        WriteFormat("%d", static_cast<int>(A.getNumber()));
+        break;
+      case 'o':
+        WriteFormat("%o", static_cast<unsigned int>(A.getNumber()));
+        break;
+      case 'x':
+      case 'X':
+        WriteFormat("%x", static_cast<unsigned int>(A.getNumber()));
+        break;
+      case 'u':
+        WriteFormat("%u", static_cast<unsigned int>(A.getNumber()));
+        break;
+      case 'f':
+      case 'F':
+        WriteFormat("%f", A.getNumber());
+        break;
+      case 'e':
+      case 'E':
+        WriteFormat("%e", A.getNumber());
+        break;
+      case 'a':
+      case 'A':
+        WriteFormat("%a", A.getNumber());
+        break;
+      case 'g':
+      case 'G':
+        WriteFormat("%g", A.getNumber());
+        break;
+      case 'h':
+        switch (*It++) {
+        default:
+          assert(0 && "Invalid format string.");
+        case 'h':
+          switch (*It++) {
+          default:
+            assert(0 && "Invalid format string.");
+          case 'd':
+          case 'i':
+            WriteFormat("%hhd", static_cast<signed char>(A.getNumber()));
+            break;
+          case 'o':
+            WriteFormat("%hho", static_cast<unsigned char>(A.getNumber()));
+            break;
+          case 'x':
+          case 'X':
+            WriteFormat("%hhx", static_cast<unsigned char>(A.getNumber()));
+            break;
+          case 'u':
+            WriteFormat("%hhu", static_cast<unsigned char>(A.getNumber()));
+          }
+          break;
+        case 'd':
+        case 'i':
+          WriteFormat("%hd", static_cast<short>(A.getNumber()));
+          break;
+        case 'o':
+          WriteFormat("%ho", static_cast<unsigned short>(A.getNumber()));
+          break;
+        case 'x':
+        case 'X':
+          WriteFormat("%hx", static_cast<unsigned short>(A.getNumber()));
+          break;
+        case 'u':
+          WriteFormat("%hu", static_cast<unsigned short>(A.getNumber()));
+        }
+        break;
+      case 'l':
+        switch (*It++) {
+        default:
+          assert(0 && "Invalid format string.");
+        case 'c':
+          WriteFormat("%lc",
+                      A.getKind() == Value::VK_Number
+                          ? static_cast<std::wint_t>(A.getNumber())
+                          : static_cast<std::wint_t>(A.getString().front()));
+          break;
+        case 's': {
+          std::wstring S;
+          std::ranges::transform(
+              A.toString(), std::back_inserter(S),
+              [](char C) { return static_cast<wchar_t>(C); });
+          WriteFormat("%ls", S.c_str());
+          break;
+        }
+        case 'd':
+        case 'i':
+          WriteFormat("%ld", static_cast<long>(A.getNumber()));
+          break;
+        case 'o':
+          WriteFormat("%lo", static_cast<unsigned long>(A.getNumber()));
+          break;
+        case 'x':
+        case 'X':
+          WriteFormat("%lx", static_cast<unsigned long>(A.getNumber()));
+          break;
+        case 'u':
+          WriteFormat("%lu", static_cast<unsigned long>(A.getNumber()));
+          break;
+        case 'f':
+        case 'F':
+          WriteFormat("%lf", A.getNumber());
+          break;
+        case 'e':
+        case 'E':
+          WriteFormat("%le", A.getNumber());
+          break;
+        case 'a':
+        case 'A':
+          WriteFormat("%la", A.getNumber());
+          break;
+        case 'g':
+        case 'G':
+          WriteFormat("%lg", A.getNumber());
+          break;
+        case 'l':
+          switch (*It++) {
+          default:
+            assert(0 && "Invalid format string.");
+          case 'd':
+          case 'i':
+            WriteFormat("%lld", static_cast<long long>(A.getNumber()));
+            break;
+          case 'o':
+            WriteFormat("%llo", static_cast<unsigned long long>(A.getNumber()));
+            break;
+          case 'x':
+          case 'X':
+            WriteFormat("%llx", static_cast<unsigned long long>(A.getNumber()));
+            break;
+          case 'u':
+            WriteFormat("%llu", static_cast<unsigned long long>(A.getNumber()));
+          }
+          break;
+        }
+        break;
+      case 'j':
+        switch (*It++) {
+        default:
+          assert(0 && "Invalid format string.");
+        case 'd':
+        case 'i':
+          WriteFormat("%jd", static_cast<std::intmax_t>(A.getNumber()));
+          break;
+        case 'o':
+          WriteFormat("%jo", static_cast<std::uintmax_t>(A.getNumber()));
+          break;
+        case 'x':
+        case 'X':
+          WriteFormat("%jx", static_cast<std::uintmax_t>(A.getNumber()));
+          break;
+        case 'u':
+          WriteFormat("%ju", static_cast<std::uintmax_t>(A.getNumber()));
+        }
+        break;
+      case 'z':
+        switch (*It++) {
+        default:
+          assert(0 && "Invalid format string.");
+        case 'd':
+        case 'i':
+          WriteFormat(
+              "%zd", static_cast<ssize_t>(A.getNumber())); // signed std::size_t
+          break;
+        case 'o':
+          WriteFormat("%zo", static_cast<std::size_t>(A.getNumber()));
+          break;
+        case 'x':
+        case 'X':
+          WriteFormat("%zx", static_cast<std::size_t>(A.getNumber()));
+          break;
+        case 'u':
+          WriteFormat("%zu", static_cast<std::size_t>(A.getNumber()));
+        }
+        break;
+      case 't':
+        switch (*It++) {
+        default:
+          assert(0 && "Invalid format string.");
+        case 'd':
+        case 'i':
+          WriteFormat("%td", static_cast<std::ptrdiff_t>(A.getNumber()));
+          break;
+        case 'o':
+          WriteFormat("%zo", static_cast<std::size_t>(
+                                 A.getNumber())); // unsigned std::ptrdiff_t
+          break;
+        case 'x':
+        case 'X':
+          WriteFormat("%zx", static_cast<std::size_t>(
+                                 A.getNumber())); // unsigned std::ptrdiff_t
+          break;
+        case 'u':
+          WriteFormat("%zu", static_cast<std::size_t>(
+                                 A.getNumber())); // unsigned std::ptrdiff_t
+        }
+        break;
+      case 'L':
+        switch (*It++) {
+        default:
+          assert(0 && "Invalid format string.");
+        case 'f':
+        case 'F':
+          WriteFormat("%Lf", static_cast<long double>(A.getNumber()));
+          break;
+        case 'e':
+        case 'E':
+          WriteFormat("%Le", static_cast<long double>(A.getNumber()));
+          break;
+        case 'a':
+        case 'A':
+          WriteFormat("%La", static_cast<long double>(A.getNumber()));
+          break;
+        case 'g':
+        case 'G':
+          WriteFormat("%Lg", static_cast<long double>(A.getNumber()));
+        }
+      }
     }
 
-    It = std::find(It, std::end(FormatString), '%');
+    auto Next = std::find(It, End, '%');
+    std::move(It, Next, BufferIt);
+    BufferIt += Next - It;
 
-    if (It != std::end(FormatString)) {
+    if (Next != End) {
       assert(0 && "Invalid format string.");
       exit(EXIT_FAILURE);
     }
 
-    return std::move(String);
+    return {std::begin(Buffer), BufferIt};
   }
 };
 
