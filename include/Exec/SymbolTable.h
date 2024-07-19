86d4e395fe094405ae2a1e1ab2f8c914cb4900ab
Andrew Sukach <andrewsukach@gmail.com>
2024-07-19 16:40:00 -0400

    Remove FunctionSymbolTable

--- include/Exec/SymbolTable.h
+++ include/Exec/SymbolTable.h
@@ -51,37 +51,3 @@ public:
       Table.emplace_back(std::string(Key), Value);
   }
 };
-
-template <typename T>
-class FunctionSymbolTable : SymbolTable<FunctionSymbolTable<T>, T> {
-public:
-  using Element = std::pair<std::string, T>;
-
-private:
-  std::vector<Element> Table;
-
-  auto lookup(std::string_view Key) const {
-    return std::ranges::find(Table, Key,
-                             [](const Element &E) { return E.first; });
-  }
-
-public:
-  bool contains(std::string_view Key) const {
-    return lookup(Key) != std::cend(Table);
-  }
-
-  T &get(std::string_view Key) const {
-    assert(contains(Key) && "Key not in Symbol table");
-    return const_cast<T &>(lookup(Key)->second);
-  }
-
-  void set(std::string_view Key, T Value) {
-    if (contains(Key))
-      get(Key) =
-          T::Create(Value.getIdentifier(), Value.getParams(), Value.getBody());
-    else
-      Table.emplace_back(
-          std::string(Key),
-          T::Create(Value.getIdentifier(), Value.getParams(), Value.getBody()));
-  }
-};
