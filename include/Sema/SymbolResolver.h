aaf948bad43a9d6e049b22d5c134b6c9aab9f3a0
Andrew Sukach <andrewsukach@gmail.com>
2024-08-02 02:47:40 -0400

    Remove SemaDecl

---
+++ include/Sema/SymbolResolver.h
@@ -0,0 +1,58 @@
+#include "AST/AST.h"
+#include "AST/ASTVisitor.h"
+
+#include <string>
+#include <string_view>
+#include <unordered_map>
+#include <unordered_set>
+#include <variant>
+
+namespace cawk {
+class SymbolResolver : ASTVisitor<Resolver, trav::Preorder, true> {
+  friend class ASTVisitor<Resolver, trav::Preorder, true>;
+
+  std::unordered_map<std::string, FunctionDecl *> FunctionResolutions;
+  std::unordered_map<std::string, DeclRefExpr *> GlobalResolutions;
+  std::unordered_map<std::string, ParamVarDecl *> LocalResolutions;
+  std::unordered_set<std::string> LocalSymbols;
+  std::vector<CallExpr *> UnresolvedSymbols;
+
+public:
+  bool check(TranslationUnitDecl *T);
+
+private:
+  bool visit(FunctionDecl *F);
+  bool visit(ParamVarDecl *P);
+  bool visit(RuleDecl *R);
+  bool visit(TranslationUnitDecl *T);
+  bool visit(VarDecl *V);
+
+  bool visit(BreakStmt *B);
+  bool visit(CompoundStmt *C);
+  bool visit(ContinueStmt *C);
+  bool visit(DeleteStmt *D);
+  bool visit(DoStmt *D);
+  bool visit(ExitStmt *E);
+  bool visit(ForStmt *F);
+  bool visit(ForRangeStmt *F);
+  bool visit(IfStmt *I);
+  bool visit(NextStmt *N);
+  bool visit(NextfileStmt *N);
+  bool visit(PrintStmt *P);
+  bool visit(ReturnStmt *R);
+  bool visit(ValueStmt *V);
+  bool visit(WhileStmt *W);
+
+  bool visit(ArraySubscriptExpr *A);
+  bool visit(BinaryOperator *B);
+  bool visit(CallExpr *C);
+  bool visit(DeclRefExpr *D);
+  bool visit(FloatingLiteral *F);
+  bool visit(RegexLiteral *R);
+  bool visit(StringLiteral *S);
+  bool visit(UnaryOperator *U);
+
+  DeclRefExpr *resolve(DeclRefExpr *D);
+};
+
+} // namespace cawk
---
+++ lib/Sema/SymbolResolver.cpp
@@ -0,0 +1,165 @@
+#include "Sema/SymbolResolver.h"
+
+#include "Support/Support.h"
+
+using namespace cawk;
+
+bool SymbolResolver::visit(FunctionDecl *F) {
+  if (!FunctionResolutions.try_emplace(std::string(F->getName()), F) ||
+      GlobalResolutions.contains(std::string(F->getName())))
+    return false;
+
+  LocalSymbols.clear();
+  LocalResolutions.clear();
+  return true;
+}
+
+bool SymbolResolver::visit(ParamVarDecl *P) {
+  if (!LocalSymbols.try_emplace(std::string(P->getName())))
+    return false;
+  return true;
+}
+
+bool SymbolResolver::visit(RuleDecl *R) {
+  LocalSymbols.clear();
+  LocalResolutions.clear();
+  if (DeclRefExpr *D = dyn_cast_or_null<DeclRefExpr>(R->getPattern()))
+    R->setPattern(resolve(D));
+  return true;
+}
+
+bool SymbolResolver::visit(TranslationUnitDecl *T) { return true; }
+
+bool SymbolResolver::visit(VarDecl *V) { return true; }
+
+bool SymbolResolver::visit(BreakStmt *B) { return true; }
+
+bool SymbolResolver::visit(CompoundStmt *C) { return true; }
+
+bool SymbolResolver::visit(ContinueStmt *C) { return true; }
+
+bool SymbolResolver::visit(DeleteStmt *D) {
+  if (DeclRefExpr *DRE = dyn_cast_or_null<DeclRefExpr>(D->getArgument()))
+    D->setArgument(resolve(DRE));
+  return true;
+}
+
+bool SymbolResolver::visit(DoStmt *D) { return true; }
+
+bool SymbolResolver::visit(ExitStmt *E) {
+  if (DeclRefExpr *D = dyn_cast_or_null<DeclRefExpr>(E->getValue()))
+    E->setValue(resolve(D));
+  return true;
+}
+
+bool SymbolResolver::visit(ForStmt *F) {
+  if (DeclRefExpr *D = dyn_cast_or_null<DeclRefExpr>(F->getCond()))
+    F->setCond(resolve(D));
+  return true;
+}
+
+bool SymbolResolver::visit(ForRangeStmt *F) {
+  F->setLoopVar(resolve(F->getLoopVar()));
+  F->setRange(resolve(F->getRange()));
+  return true;
+}
+
+bool SymbolResolver::visit(IfStmt *I) {
+  if (DeclRefExpr *D = dyn_cast_or_null<DeclRefExpr>(I->getCond()))
+    I->setCond(resolve(D));
+  return true;
+}
+
+bool SymbolResolver::visit(NextStmt *N) { return true; }
+
+bool SymbolResolver::visit(NextfileStmt *N) { return true; }
+
+bool SymbolResolver::visit(PrintStmt *P) {
+  for (int I = 0; I != std::size(P->getArgs()); ++I)
+    if (DeclRefExpr *D = dyn_cast_or_null<DeclRefExpr>(P->getArgs()[I]))
+      P->setArg(I, resolve(D));
+  return true;
+}
+
+bool SymbolResolver::visit(ReturnStmt *R) {
+  if (DeclRefExpr *D = dyn_cast_or_null<DeclRefExpr>(R->getValue()))
+    R->setValue(D);
+  return true;
+}
+
+bool SymbolResolver::visit(ValueStmt *V) {
+  if (DeclRefExpr *D = dyn_cast_or_null<DeclRefExpr>(V->getValue()))
+    V->setValue(D);
+  return true;
+}
+
+bool SymbolResolver::visit(WhileStmt *W) {
+  if (DeclRefExpr *D = dyn_cast_or_null<DeclRefExpr>(W->getValue()))
+    W->setCond(D);
+  return true;
+}
+
+bool SymbolResolver::visit(ArraySubscriptExpr *A) {
+  if (DeclRefExpr *D = dyn_cast_or_null<DeclRefExpr>(A->getLHS()))
+    A->setLHS(D);
+  for (int I = 0; I != std::size(A->getRHS()); ++I)
+    if (DeclRefExpr *D = dyn_cast_or_null<DeclRefExpr>(A->getRHS()[I]))
+      A->setRHS(I, D);
+  return true;
+}
+
+bool SymbolResolver::visit(BinaryOperator *B) {
+  if (DeclRefExpr *D = dyn_cast_or_null<DeclRefExpr>(B->getLHS()))
+    B->setLHS(D);
+  if (DeclRefExpr *D = dyn_cast_or_null<DeclRefExpr>(B->getRHS()))
+    B->setRHS(D);
+  return true;
+}
+
+bool SymbolResolver::visit(CallExpr *C) {
+  assert(isa<DeclRefExpr>(C->getCallee()));
+  std::string Name(static_cast<DeclRefExpr *>(C->getCallee())->getName());
+  if (FunctionResolutions.contains(Name)) {
+    C->setFunction(FunctionResolutions[Name]);
+  } else {
+    UnresolvedSymbols.push_back(C);
+  }
+
+  for (int I = 0; I != std::size(C->getArgs()); ++I)
+    if (DeclRefExpr *D = dyn_cast_or_null<DeclRefExpr>(B->getRHS()))
+      C->setArg(I, D);
+
+  return true;
+}
+
+bool SymbolResolver::visit(DeclRefExpr *D) { return true; }
+
+bool SymbolResolver::visit(FloatingLiteral *F) { return true; }
+
+bool SymbolResolver::visit(RegexLiteral *R) { return true; }
+
+bool SymbolResolver::visit(StringLiteral *S) { return true; }
+
+bool SymbolResolver::visit(UnaryOperator *U) { return true; }
+
+bool SymbolResolver::check(TranslationUnitDecl *T) {
+  if (!visit(T))
+    return false;
+
+  for (CallExpr *C : UnresolvedSymbols)
+    if (!FunctionResolutions.contains(
+            ptr_cast<DeclRefExpr>(C->getCallee())->getName()))
+      return false;
+    else
+      C->setFunction(ptr_cast<DeclRefExpr>(C->getCallee())->getName());
+
+  return true;
+}
+
+DeclRefExpr *SymbolResolver::resolve(DeclRefExpr *D) {
+  auto Name = D->getName();
+  if (LocalSymbols.contains(Name))
+    return LocalResolutions.try_emplace(Name, D);
+  assert(!FunctionResolutions.contains(Name));
+  return GlobalResolutions.try_emplace(Name, D);
+}
