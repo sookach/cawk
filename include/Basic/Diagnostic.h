1498411ed5ad2ad9fd837d5b3f22732566f435cd
Andrew Sukach <andrewsukach@gmail.com>
2024-08-18 16:41:31 -0400

    Beef up Diagnostics

--- include/Basic/Diagnostic.h
+++ include/Basic/Diagnostic.h
@@ -1,6 +1,8 @@
 #pragma once
 
+#include <algorithm>
 #include <array>
+#include <cassert>
 #include <cstdio>
 #include <string>
 #include <vector>
@@ -16,28 +18,54 @@ std::string_view getDiagnosticText(diag::DiagnosticKind Kind);
 } // namespace diag
 
 class Diagnostic {
-  std::vector<std::string> ErrorDiagnostics;
-  std::vector<std::string> WarningDiagnostics;
+  using DiagVector = std::vector<std::pair<std::size_t, std::string>>;
+  DiagVector ErrorDiagnostics;
+  DiagVector WarningDiagnostics;
+
+  std::string formatDiagnostic(std::string_view Text) {
+    assert(!std::ranges::contains(Text, '%') && "missing format argument");
+    return std::string(Text);
+  }
+
+  template <typename T, typename... ArgsTy>
+  std::string formatDiagnostic(std::string_view Text, T &&Arg,
+                               ArgsTy &&...Args) {
+    auto I = std::ranges::find(Text, '%');
+    assert(I != std::cend(Text) && "missing format specifier");
+    std::string FormattedText(std::cbegin(Text), I);
+    if constexpr (std::is_same_v<std::decay_t<T>, std::string>)
+      FormattedText += Arg;
+    else if constexpr (std::is_same_v<std::decay_t<T>, const char *>)
+      FormattedText += std::string(Arg);
+    else
+      FormattedText += std::to_string(Arg);
+    return FormattedText +
+           formatDiagnostic(std::string_view(I + 1, std::cend(Text)),
+                            std::forward<ArgsTy>(Args)...);
+  }
 
   template <typename... T>
-  void addDiagnostic(std::vector<std::string> &Diagnostics,
+  std::string formatDiagnostic(diag::DiagnosticKind Kind, T &&...Args) {
+    return formatDiagnostic(diag::getDiagnosticText(Kind),
+                            std::forward<T>(Args)...);
+  }
+
+  template <typename... T>
+  void addDiagnostic(DiagVector &Diagnostics, std::size_t Line,
                      diag::DiagnosticKind Kind, T &&...Args) {
-    std::array<char, 1'024> Buffer;
-    auto N = std::snprintf(std::begin(Buffer), std::size(Buffer),
-                           diag::getDiagnosticText(Kind).data(),
-                           std::forward<T>(Args)...);
-    Diagnostics.emplace_back(std::cbegin(Buffer), std::cbegin(Buffer) + N);
+    Diagnostics.emplace_back(Line,
+                             formatDiagnostic(Kind, std::forward<T>(Args)...));
   }
 
 public:
   template <typename... T>
-  void addError(diag::DiagnosticKind Kind, T &&...Args) {
-    addDiagnostic(ErrorDiagnostics, Kind, std::forward<T>(Args)...);
+  void addError(std::size_t Line, diag::DiagnosticKind Kind, T &&...Args) {
+    addDiagnostic(ErrorDiagnostics, Line, Kind, std::forward<T>(Args)...);
   }
 
   template <typename... T>
-  void addWarning(diag::DiagnosticKind Kind, T &&...Args) {
-    addDiagnostic(WarningDiagnostics, Kind, std::forward<T>(Args)...);
+  void addWarning(std::size_t Line, diag::DiagnosticKind Kind, T &&...Args) {
+    addDiagnostic(WarningDiagnostics, Line, Kind, std::forward<T>(Args)...);
   }
 };
 } // namespace cawk
