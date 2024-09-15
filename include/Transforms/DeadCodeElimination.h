2977c827dd0a4525a93d32d73c48d2183323af35
Andrew Sukach <andrewsukach@gmail.com>
2024-09-14 22:57:36 -0400

    Dead Code Elimination pass

---
+++ include/Transforms/DeadCodeElimination.h
@@ -0,0 +1,49 @@
+#include "AST/AST.h"
+#include "AST/ASTVisitor.h"
+
+namespace cawk {
+class DCEPass : public ASTVisitor<DCEPass, trav::Preorder, true> {
+  friend class ASTVisitor<DCEPass, trav::Preorder, true>;
+
+  bool visit(CompoundStmt *S) {
+    std::vector<Stmt *> Body;
+    for (Stmt *S : S->getBody())
+      if (!isa<NullStmt>(S))
+        Body.push_back(S);
+    S->setBody(Body);
+    return true;
+  }
+
+  bool visit(DoStmt *S) {
+    if (isa<NullStmt>(S->getBody()))
+      S->setBody(nullptr);
+    return true;
+  }
+
+  bool visit(ForStmt *S) {
+    if (isa<NullStmt>(S->getBody()))
+      S->setBody(nullptr);
+    return true;
+  }
+
+  bool visit(ForRangeStmt *S) {
+    if (isa<NullStmt>(S->getBody()))
+      S->setBody(nullptr);
+    return true;
+  }
+
+  bool visit(IfStmt *S) {
+    if (isa<NullStmt>(S->getThen()))
+      S->setThen(nullptr);
+    if (S->getElse() != nullptr && isa<NullStmt>(S->getElse()))
+      S->setElse(nullptr);
+    return true;
+  }
+
+  bool visit(WhileStmt *S) {
+    if (isa<NullStmt>(S->getBody()))
+      S->setBody(nullptr);
+    return true;
+  }
+};
+} // namespace cawk
