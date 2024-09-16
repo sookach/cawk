705c9b3e6d59464a5139546a7a0681ac51b9b861
Andrew Sukach <andrewsukach@gmail.com>
2024-09-16 16:32:35 -0400

    Formatting; NFC

--- cawk/include/IR/Instruction.h
+++ cawk/include/IR/Instruction.h
@@ -1,23 +1,25 @@
 #pragma once
 
-#include <vector>
 #include <cstdint>
+#include <vector>
+
+#include "Exec/IO.h"
 
 namespace cawk {
-  namespace inst {
-  enum InstKind : unsigned {
+namespace inst {
+enum InstKind : unsigned {
 #define INST(ID, SP) ID,
 #include "IR/Instruction.def"
-  };
+};
 
-  std::string_view getInstructionName(InstKind Kind) {
-    switch (Kind) {
+std::string_view getInstructionName(InstKind Kind) {
+  switch (Kind) {
 #define INST(ID, SP)                                                           \
   case ID:                                                                     \
     return #SP;
 #include "IR/Instruction.def"
 #undef INST
-    }
   }
-  } // namespace inst
 }
+} // namespace inst
+} // namespace cawk
